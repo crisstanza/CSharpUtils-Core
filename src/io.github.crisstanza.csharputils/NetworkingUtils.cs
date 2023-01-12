d0864ff525b00d7d0a565d0135372c8c7ed877a7
Cris Stanza <crisstanza@users.noreply.github.com>
2023-01-12 20:59:04 -0300

    Fix (workaround) for NetworkInterface.GetAllNetworkInterfaces( ) exception.

--- src/io.github.crisstanza.csharputils/NetworkingUtils.cs
+++ src/io.github.crisstanza.csharputils/NetworkingUtils.cs
@@ -1,27 +1,39 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 
 namespace io.github.crisstanza.csharputils
 {
-	public class NetworkingUtils
-	{
-		public IPAddress GetLocalAddress(IPAddress subnetMask)
-		{
-			foreach (NetworkInterface adapter in NetworkInterface.GetAllNetworkInterfaces())
-			{
-				foreach (UnicastIPAddressInformation unicastIPAddressInformation in adapter.GetIPProperties().UnicastAddresses)
-				{
-					if (unicastIPAddressInformation.Address.AddressFamily == AddressFamily.InterNetwork)
-					{
-						if (unicastIPAddressInformation.IPv4Mask.Equals(subnetMask))
-						{
-							return unicastIPAddressInformation.Address;
-						}
+    public class NetworkingUtils
+    {
+        public IPAddress GetLocalAddress(IPAddress subnetMask)
+        {
+			// TODO (Cris Stanza): check command: hostname -I (see firs result)
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+					if (ip.ToString().StartsWith("192.")) {
+                    	return ip;
 					}
-				}
-			}
-			return null;
-		}
-	}
+                }
+            }
+            foreach (NetworkInterface adapter in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                foreach (UnicastIPAddressInformation unicastIPAddressInformation in adapter.GetIPProperties().UnicastAddresses)
+                {
+                    if (unicastIPAddressInformation.Address.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        if (unicastIPAddressInformation.IPv4Mask.Equals(subnetMask))
+                        {
+                            return unicastIPAddressInformation.Address;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
 }
