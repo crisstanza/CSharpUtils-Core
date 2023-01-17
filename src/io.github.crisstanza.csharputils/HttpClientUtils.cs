448732666c1f50489fea56f1d574bdcdc31cb956
Cris Stanza <crisstanza@users.noreply.github.com>
2023-01-16 21:10:12 -0300

    HTTP get.

--- src/io.github.crisstanza.csharputils/HttpClientUtils.cs
+++ src/io.github.crisstanza.csharputils/HttpClientUtils.cs
@@ -2,39 +2,53 @@
 using System;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace io.github.crisstanza.csharputils
 {
-	public class HttpClientUtils
-	{
-		private readonly string requestUri;
-		private readonly StringUtils stringUtils;
-		public HttpClientUtils(string requestUri)
-		{
-			this.requestUri = requestUri;
-			this.stringUtils = new StringUtils();
-		}
-		public void PostJson(string json)
-		{
-			Console.WriteLine("[PostJson] " + json + " [/PostJson]");
-			if (this.stringUtils.IsBlank(this.requestUri))
-			{
-				Console.WriteLine("[PostJson] no requestUri [/PostJson]");
-			}
-			else
-			{
-				HttpClient client = new HttpClient();
-				HttpContent content = new StringContent(json, Encoding.UTF8, MediaTypeNamesConstants.APPLICATION_JSON);
-				Console.WriteLine("[PostJson] Posting to " + this.requestUri + " [/PostJson]");
-				try
-				{
-					client.PostAsync(this.requestUri, content);
-				}
-				catch (InvalidOperationException exc)
-				{
-					Console.WriteLine(exc.Message);
-				}
-			}
-		}
-	}
+    public class HttpClientUtils
+    {
+        private readonly HttpClient client = new HttpClient();
+        private readonly string requestUri;
+        private readonly StringUtils stringUtils;
+        public HttpClientUtils(string requestUri)
+        {
+            this.client = new HttpClient();
+            this.requestUri = requestUri;
+            this.stringUtils = new StringUtils();
+        }
+        public void PostJson(string json)
+        {
+            HttpContent content = new StringContent(json, Encoding.UTF8, MediaTypeNamesConstants.APPLICATION_JSON);
+            this.client.PostAsync(this.requestUri, content);
+        }
+        public string Get()
+        {
+            Task<string> result = this.Get_();
+            if (result == null)
+            {
+                return null;
+            }
+            return result.Result;
+        }
+
+        private async Task<string> Get_()
+        {
+            try
+            {
+                using HttpResponseMessage response = await this.client.GetAsync(this.requestUri);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (InvalidOperationException exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            catch (HttpRequestException exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            return null;
+        }
+    }
 }
